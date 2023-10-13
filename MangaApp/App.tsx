80b8608097f676bb4506fc28774d0dcd34bb04fc
lhhdai06022003 <lhhd06022003@gmail.com>
2023-10-13 15:06:14 +0700

    init detailScren

--- MangaApp/App.tsx
+++ MangaApp/App.tsx
@@ -3,17 +3,19 @@ import React from 'react'
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import BottomNavigations from './src/navigators/BottomNavigations';
+import DetailScreen from './src/screens/DetailScreen';
 
 
 const Stack = createNativeStackNavigator();
 
 const App = () => {
     return (
-      <NavigationContainer>
-        <Stack.Navigator screenOptions={{headerShown: false}}>
-          <Stack.Screen name="Bottom" component={BottomNavigations} options={{animation:'default'}} />             
-        </Stack.Navigator>
-      </NavigationContainer>
+      // <NavigationContainer>
+      //   <Stack.Navigator screenOptions={{headerShown: false}}>
+      //     <Stack.Screen name="Bottom" component={BottomNavigations} options={{animation:'default'}} />             
+      //   </Stack.Navigator>
+      // </NavigationContainer>
+      <DetailScreen />
     )
   
 }
--- MangaApp/src/screens/DetailScreen.tsx
+++ MangaApp/src/screens/DetailScreen.tsx
@@ -1,14 +1,17 @@
-import { ImageBackground, ScrollView, StyleSheet, Text, View } from 'react-native'
+import { Image,ImageBackground, ScrollView, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
 const UserAccountScreen = () => {
   return (
-    <SafeAreaView style={{backgroundColor:'white',paddingHorizontal:16,paddingTop:8}}>
-         <ScrollView showsVerticalScrollIndicator = {false}>
-          <ImageBackground  blurRadius={5} resizeMode='contain' source={require('../assets/images/avatar.png')}/>
-            
-         </ScrollView>
+    <SafeAreaView style={{backgroundColor:'white',flex:1}}>
+            <ImageBackground  style={{flex:1}} blurRadius={5}  source={require('../assets/images/testimage.jpg')}>
+              <Image  style={{position:'absolute',height:100,width:100}}  resizeMode='contain' source={require('../assets/images/testimage.jpg')}/>
+            </ImageBackground>
+           
+          
+          
+         
     </SafeAreaView>
   )
 }
