99ce32310649b3c6a65e75ec5110b8d5ed98913e
leviettinh1479 <115515237+leviettinh1479@users.noreply.github.com>
2023-10-24 10:48:06 +0700

    Done add manga

--- Server/index.js
+++ Server/index.js
@@ -14,6 +14,7 @@ const genreRouter = require("./routes/genre");
 const reportRouter = require("./routes/report");
 const favoriteRouter = require("./routes/favorite");
 const mangaCpanel = require("./cpanel/manga");
+const genreCpanel = require("./cpanel/genre");
 
 // INIT
 const PORT = process.env.PORT || 3000;
@@ -42,6 +43,7 @@ app.use("/api/favorite", favoriteRouter);
 app.use(genreRouter);
 app.use(reportRouter);
 app.use(mangaCpanel);
+app.use(genreCpanel);
 
 
 
